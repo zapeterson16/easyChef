686173fe122d45c31f6d7dcf28c9f91dd1ff483e
stevenhuang119 <31111901+stevenhuang119@users.noreply.github.com>
2017-09-23 19:57:47 -0400

    Utilized NSUserDefault to store data

--- easyChef/SecondViewController.swift
+++ easyChef/SecondViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
-var protein = 0.0
-var carbs = 0.0
-var fat = 0.0
 class SecondViewController: UIViewController {
+    /*
+    var protein = 0.0
+    var carbs = 0.0
+    var fat = 0.0
+    */
+ 
+    let myDefaults = UserDefaults.init()
+    
+    let protein_calorie_input = "protein_calorie_input"
+    let carb_calorie_input = "carb_calorie_input"
+    let fat_calorie_input = "fat_calorie_input"
     
     @IBOutlet weak var fat_input: UITextField!
     @IBOutlet weak var carb_input: UITextField!
     @IBOutlet weak var protein_input: UITextField!
+    @IBOutlet weak var sanity_check: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,19 +34,22 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Action of the three buttons, saving the values into the NSUserDafault
     @IBAction func set_protein_input(_ sender: UIButton) {
         let myDouble = Double(protein_input.text!)
-        protein = myDouble!
+        myDefaults.set(myDouble, forKey:protein_calorie_input)
     }
     @IBAction func set_carb_input(_ sender: UIButton) {
         let myDouble = Double(carb_input.text!)
-        carbs = myDouble!
+        myDefaults.set(myDouble, forKey:carb_calorie_input)
     }
     @IBAction func set_fat_input(_ sender: UIButton) {
         let myDouble = Double(fat_input.text!)
-        fat = myDouble!
+        myDefaults.set(myDouble, forKey:fat_calorie_input)
     }
-    
+    @IBAction func check(_ sender: UIButton) {
+        sanity_check.text = String(myDefaults.double(forKey: protein_calorie_input))
+    }
 }
 
