d9a543a79c07c007da045a80bc957984bc6a2aec
zachp <zachp@umich.edu>
2017-09-23 17:06:32 -0400

    first view controller changed

--- easyChef/FirstViewController.swift
+++ easyChef/FirstViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameMealLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        nameMealLabel.text = "Default text"
+        
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        nameMealLabel.text = textField.text
+    }
+    
+    
 }
 
---
+++ easyChef/Ingredient.swift
@@ -0,0 +1,27 @@
+//
+//  Ingredient.swift
+//  easyChef
+//
+//  Created by Zachary Peterson on 9/23/17.
+//  Copyright © 2017 Zachary Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ingredient{
+    var name: String
+    var protein: Int
+    var carbs: Int
+    var fat: Int
+    var servingSize: Int //in grams
+    
+    init(name: String, protein: Int, carbs: Int, fat: Int, servingSize: Int){
+        self.name = name
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
+        self.servingSize = servingSize
+        
+        
+    }
+}
