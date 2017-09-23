390c219a35d17bd9be3a1a6c2247c0ff90e10964
zachp <zachp@umich.edu>
2017-09-23 17:18:21 -0400

    Added third view controller file

---
+++ easyChef/ThirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdViewController.swift
+//  easyChef
+//
+//  Created by Zachary Peterson on 9/23/17.
+//  Copyright © 2017 Zachary Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
